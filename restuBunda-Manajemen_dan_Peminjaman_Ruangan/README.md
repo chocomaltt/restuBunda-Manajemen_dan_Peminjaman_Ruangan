b20a1bfc08545e2cc7f46d73026af3d3bca70797
Reza Arya Wijaya <114033542+chocomaltt@users.noreply.github.com>
2023-11-29 10:32:17 +0700

    Delete restuBunda-Manajemen_dan_Peminjaman_Ruangan directory

--- restuBunda-Manajemen_dan_Peminjaman_Ruangan/README.md
+++
@@ -1,5 +0,0 @@
-# restuBunda-Manajemen_dan_Peminjaman_Ruangan
----
-### Repository Kelompok 4 restuBunda
-Repository untuk tugas akhir Semester 3
-
--- restuBunda-Manajemen_dan_Peminjaman_Ruangan/config/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set("Asia/Jakarta");
-$koneksi = mysqli_connect("localhost", "root","","nyeleh");
-
-if(mysqli_connect_errno()){
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
-?>
