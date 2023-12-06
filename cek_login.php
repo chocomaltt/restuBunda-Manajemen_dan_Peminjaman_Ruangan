76aef1bef35e6854cbdeeae969162d5588ff2017
amandafadilaerros <amandafadila1@gmail.com>
2023-12-06 09:23:42 +0700

    login, logout, index, fungsi, read data

--- cek_login.php
+++ cek_login.php
@@ -1,6 +1,7 @@
-     <?php
-if(session_status() === PHP_SESSION_NONE)
+<?php
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
+}
 
 include "config/koneksi.php";
 include "function/pesan_kilat.php";
@@ -9,26 +10,34 @@ include "function/anti_injection.php";
 $username = antiinjection($koneksi, $_POST['username']);
 $password = antiinjection($koneksi, $_POST['password']);
 
-$query = "SELECT username, level, salt, password as hashed_password FROM user WHERE username = '$username'";
+$query = "SELECT Username, LevelID, Salt, Password as hashed_password FROM Akun WHERE Username = '$username'";
 $result = mysqli_query($koneksi, $query);
+
+// Tambahkan kode untuk menampilkan query jika terjadi kesalahan
+if (!$result) {
+    die('Error in query: ' . mysqli_error($koneksi));
+}
+
 $row = mysqli_fetch_assoc($result);
 mysqli_close($koneksi);
-$salt = $row['salt'];
-$hashed_password = $row['hashed_password'];
-header("Location: index.php");
-if($salt !== null && $hashed_password !== null){
-    $combined_password  = $salt . $password;
 
-    if(password_verify($combined_password, $hashed_password)){
-        $_SESSION['username'] = $row['username'];
-        $_SESSION['level'] = $row['level'];
-        header("Location: index.php");
+$salt = $row['Salt'];
+$hashed_password = $row['hashed_password'];
+
+if ($salt !== null && $hashed_password !== null) {
+    $combined_password = $salt . $password;
+    if (password_verify($combined_password, $hashed_password)) {
+        $_SESSION['username'] = $row['Username']; // Perbaiki penulisan kolom 'Username'
+        $_SESSION['level'] = $row['LevelID'];    // Perbaiki penulisan kolom 'LevelID'
+        header("location:index.php");
     } else {
-        pesan('danger', "Login gagal. Password Anda Salah.");
-        header("Location: login.php");
+        // pesan('danger', "Login gagal. Password Anda Salah.");
+        // header("Location: login.php");
+        echo "Login gagal. Password Anda Salah.";
     }
 } else {
-    pesan('warning', "Username tidak ditemukan.");
-    header("Location: login.php");
+    // pesan('warning', "Username tidak ditemukan.");
+    // header("Location: login.php");
+    echo "Username tidak ditemukan.";
 }
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
+
 if(session_status() === PHP_SESSION_NONE)
     session_start();
 
 if(!empty($_SESSION['level'])){
     require 'config/koneksi.php';
     require 'function/pesan_kilat.php';
-    include 'admin/template/header.php';
-    if(!empty($_GET['page'])){
-        include 'admin/module/' . $_GET['page'] . '/index.php';
-    } else {
-        include 'admin/template/home.php';
-    }
-    include 'admin/template/footer.php';
-} else {
+    // require 'function/fungsi.php';
+    if($_SESSION['level']==1){
+        include 'view/admin/template/sidebar.php';
+        if(!empty($_GET['page'])){
+            include 'view/admin/' . $_GET['page'];
+        } else {
+            include 'view/admin/beranda.php';
+        }
+        include 'view/admin/template/footer.php';
+    } else if ($_SESSION['level']==2||$_SESSION['level']==3){
+        include 'view/user/template/header.php';
+            if(!empty($_GET['page'])){
+                include 'view/user/template/' . $_GET['page'];
+            } else {
+                include 'view/user/template/beranda.php';
+            }
+            include 'view/user/template/footer.php';
+        }
+    }else {
     header("Location: login.php");
 }
 ?>
