cceba25ed262d19bdecb3b11a8e4e9ea57e86b1c
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2023-12-09 13:18:36 +0700

    fixing bug denah ruangan href

--- view/user/template/sidebar.php
+++ view/user/template/sidebar.php
@@ -19,7 +19,6 @@
             <img src="assets/img/logo_jti_baru.png" alt="logo JTI Polinema" style="width: 54px; height: 57px;">
         </div>
         <div class="d-flex flex-column justify-content-start mx-1 gap-3">
-            <a href="index.php?page=daftar-ruang.php">test button</a>
             <ul class="list-group">
                 <li class="list-group-item hover-effect" style="border: none;">
                 <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-1">
@@ -34,9 +33,9 @@
             <div class="collapse mx-4" id="collapseInformasi">
                 <ul class="list-group gap-2">
                         <li class="list-group-item hover-effect" style="border-style: none;" >
-                        <a href="index.php?page=daftar-ruang.php" class="link-dark link-underline link-underline-opacity-0" onclick="activateMenu(event)">
+                        <a href="index.php?page=daftar-ruang.php" class="link-dark link-underline link-underline-opacity-0">
                         <img src="assets/img/daftar-ruangan.png" alt="" class="pb-1">
-                        <span class="ms-2">Daftar Ruang</span></a></li>
+                        <span class="ms-2" onclick="activateMenu(event);">Daftar Ruang</span></a></li>
 
                         <li class="list-group-item hover-effect" style="border-style: none;" >
                         <a href="index.php?page=jadwal-ruang.php" class="link-dark link-underline link-underline-opacity-0" onclick="activateMenu(event)">
@@ -44,7 +43,7 @@
                         <span class="ms-2">Jadwal Ruang</span></a></li>
 
                         <li class="list-group-item hover-effect" style="border-style: none;" >
-                        <a href="#" class="link-dark link-underline link-underline-opacity-0" onclick="activateMenu(event)">
+                        <a href="index.php?page=denah-ruang.php" class="link-dark link-underline link-underline-opacity-0" onclick="activateMenu(event)">
                         <img src="assets/img/house-plan 1.png" alt="" class="pb-1">
                         <span class="ms-2">Denah Ruang</span></a></li>
                 </ul>
@@ -55,23 +54,23 @@
             <div class="collapse mx-4" id="collapsePeminjaman">
                 <ul class="list-group gap-2">
                     <li class="list-group-item hover-effect" style="border-style: none;">
-                    <a href="#" class="link-dark link-underline link-underline-opacity-0">
+                    <a href="index.php?page=pinjam-ruangan.php" class="link-dark link-underline link-underline-opacity-0">
                     <img src="assets/img/pinjam.png" alt="" class="pb-1">
                     <span class="ms-2">Pinjam Ruangan</span></a></li>
 
                     <li class="list-group-item hover-effect" style="border-style: none;">
-                        <a href="#" class="link-dark link-underline link-underline-opacity-0">
+                        <a href="index.php?page=antrean-pinjam.php" class="link-dark link-underline link-underline-opacity-0">
                         <svg width="17" height="17" viewBox="0 0 11 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-1">
                         <path d="M1.68558 13.5249C1.35975 13.5249 1.08775 13.4158 0.869584 13.1977C0.650945 12.979 0.541626 12.7068 0.541626 12.3809V1.91886C0.541626 1.59303 0.650945 1.32103 0.869584 1.10286C1.08775 0.884222 1.35975 0.774902 1.68558 0.774902H9.31433C9.64017 0.774902 9.91217 0.884222 10.1303 1.10286C10.349 1.32103 10.4583 1.59303 10.4583 1.91886V12.3809C10.4583 12.7068 10.3492 12.9788 10.131 13.1969C9.9124 13.4156 9.64017 13.5249 9.31433 13.5249H1.68558ZM1.68558 12.8166H9.31433C9.42295 12.8166 9.52282 12.7712 9.61396 12.6806C9.70463 12.5894 9.74996 12.4896 9.74996 12.3809V1.91886C9.74996 1.81025 9.70463 1.71037 9.61396 1.61924C9.52282 1.52857 9.42295 1.48324 9.31433 1.48324H8.33329V5.81469L6.91663 4.97036L5.49996 5.81469V1.48324H1.68558C1.57697 1.48324 1.4771 1.52857 1.38596 1.61924C1.29529 1.71037 1.24996 1.81025 1.24996 1.91886V12.3809C1.24996 12.4896 1.29529 12.5894 1.38596 12.6806C1.4771 12.7712 1.57697 12.8166 1.68558 12.8166Z" fill="#10375C"/>
                         </svg><span class="ms-2">Antrean Peminjaman</span></a></li>
                         
                     <li class="list-group-item hover-effect" style="border-style: none;">
-                    <a href="#" class="link-dark link-underline link-underline-opacity-0" style="border-style: none;">
+                    <a href="index.php?page=pengembalian.php" class="link-dark link-underline link-underline-opacity-0" style="border-style: none;">
                     <img src="assets/img/borrow 1.png" alt="" class="pb-1">
                     <span class="ms-2">Pengembalian</span></a></li>
 
                     <li class="list-group-item hover-effect" style="border-style: none;">
-                    <a href="#" class="link-dark link-underline link-underline-opacity-0">
+                    <a href="index.php?page=riwayat.php" class="link-dark link-underline link-underline-opacity-0">
                     <img src="assets/img/return 1.png" alt="" class="pb-1">
                     <span class="ms-2">Riwayat</span></a></li>
                 </ul>
@@ -90,6 +89,11 @@
                     });
 
                     this.classList.add('aktif');
+
+                    const href = this.querySelector('a').getAttribute('href');
+                    if (href) {
+                    window.location.href = href;
+                    }
                 });
             });
         }
@@ -98,6 +102,7 @@
             event.preventDefault();
             const menuItem = event.target.closest('.list-group-item');
             menuItem.click();
+            console.log("Menu aktif");
         }
 
         // <button onclick="sendPage('ruang1')">Ruang 1</button>
