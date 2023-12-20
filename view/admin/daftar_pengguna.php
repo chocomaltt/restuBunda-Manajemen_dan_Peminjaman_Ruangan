00e253f629ea19e7aeb85d68fe36defcb65cf00f
amandafadilaerros <amandafadila1@gmail.com>
2023-12-20 19:50:08 +0700

    daftar pengguna controller + search

--- view/admin/daftar_pengguna.php
+++ view/admin/daftar_pengguna.php
@@ -11,13 +11,15 @@
             </h1>
         </div>
         <div class="cari" style="display: flex; gap: 16px; align-items: center;">
-            <input class="search-box" type="text" placeholder=" Cari Pengguna">
-            <button class="search-button">Cari</button>
-            <!-- <button class="tambah">Tambah</button> -->
-            <button type="button" class="tambah" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                data-bs-whatever="@mdo">
-                <i class="bi bi-person-add"></i>Tambah
-            </button>
+            <form method="post" action="">
+                <input class="search-box" type="text" name="keyword" placeholder=" Cari Pengguna">
+                <button class="search-button" name="search">Cari</button>
+                <!-- <button class="tambah">Tambah</button> -->
+                <button type="button" class="tambah" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    data-bs-whatever="@mdo">
+                    <i class="bi bi-person-add"></i>Tambah
+                </button>
+            </form>
         </div>
         <table class="table" style="table-layout: auto;">
             <thead>
@@ -31,42 +33,62 @@
             </thead>
             <tbody>
                 <?php
-                        $no = 1;
-                        $joinConditions = array(
-                            "level" => "akun.LevelID = level.LevelID",
-                            "kelas" => "akun.KelasID = kelas.KelasID"
-                        );
-                        
-                        $query = readData($koneksi, "akun", '', $joinConditions);
-                        
-                        if(!empty($query)) {
-                            foreach ($query as $row){
-                                ?>
-                <tr>
-                    <!-- <th scope="row"><?= $no++; ?></th> -->
-                    <td><?= $row['AkunID']; ?></td>
-                    <!-- <td><?= $row['Username']; ?></td> -->
-                    <td><?= $row['Nama']; ?></td>
-                    <td><?= $row['NamaKelas'] ?></td>
-                    <td><?= $row['Keterangan']; ?></td>
-                    <td>
-                        <!-- <a href="index.php?page=anggota/edit&id=<?php echo $row['AkunID']; ?>"
+                $no = 1;
+                $joinConditions = array(
+                    "level" => "akun.LevelID = level.LevelID",
+                    "kelas" => "akun.KelasID = kelas.KelasID"
+                );
+                if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['search'])) {
+                    $keyword = $_POST['keyword'];
+                        $searchConditions = "(
+                                    akun.Nama LIKE '%$keyword%' OR
+                                    akun.UserName LIKE '%$keyword%'
+                                    )";
+
+                        $query = readData($koneksi, "akun", '',$joinConditions, $searchConditions);
+                    
+
+                } else {
+                    // If the form is not submitted, fetch all data
+                    $query = readData($koneksi, "akun", '', $joinConditions);
+                }
+
+                if (!empty($query)) {
+                    foreach ($query as $row) {
+                        ?>
+                        <tr>
+                            <!-- <th scope="row"><?= $no++; ?></th> -->
+                            <td>
+                                <?= $row['AkunID']; ?>
+                            </td>
+                            <!-- <td><?= $row['Username']; ?></td> -->
+                            <td>
+                                <?= $row['Nama']; ?>
+                            </td>
+                            <td>
+                                <?= $row['NamaKelas'] ?>
+                            </td>
+                            <td>
+                                <?= $row['Keterangan']; ?>
+                            </td>
+                            <td>
+                                <!-- <a href="index.php?page=anggota/edit&id=<?php echo $row['AkunID']; ?>"
                             class="btn btn-warning btn-xs"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>
                             Edit</a> -->
-                        <a role="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editModal"
-                            data-bs-whatever="@mdo">Edit</a>
-                        <a href="fungsi/hapus.php?anggota=hapus&id=<?php echo $row['AkunID']; ?>"
-                            onclick="javascript:return confirm('Hapus Data Anggota ?');"
-                            class="btn btn-danger btn-xs"><i class="fa fa-trash-o" aria-hidden="true"></i> Hapus</a>
-                    </td>
-                    <?php
-                            }
-                        } else {
-                            ?>
-                    <td colspan="4">Tidak Ada Data Tersedia</td>
-                    <?php
-                            }
-                            ?>
+                                <a role="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editModal"
+                                    data-bs-whatever="@mdo">Edit</a>
+                                <a href="fungsi/hapus.php?anggota=hapus&id=<?php echo $row['AkunID']; ?>"
+                                    onclick="javascript:return confirm('Hapus Data Anggota ?');"
+                                    class="btn btn-danger btn-xs"><i class="fa fa-trash-o" aria-hidden="true"></i> Hapus</a>
+                            </td>
+                            <?php
+                    }
+                } else {
+                    ?>
+                        <td colspan="4">Tidak Ada Data Tersedia</td>
+                        <?php
+                }
+                ?>
                 </tr>
             </tbody>
         </table>
@@ -80,25 +102,53 @@
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Pengguna</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="../../function/tambah.php?user=tambah" method="post">
+            <form action="index.php?page=controller/daftar_pengguna.php&aksi=tambah" method="post">
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="id" class="col-form-label">Nama Pengguna :</label>
-                        <input type="text" name="idUser" class="form-control" id="id">
-                    </div>
-                    <div class=" mb-3">
-                        <label for="id" class="col-form-label">Kelas :</label>
-                        <input type="text" name="passUser" class="form-control" id="id">
+                        <input type="hidden" name="data[]" class="form-control" value="1">
+                        <input type="text" name="data[]" class="form-control" id="id" required>
                     </div>
                     <div class="mb-3">
                         <label for="id" class="col-form-label">Level :</label>
                         <!-- <input type="text" name="passUser" class="form-control" id="id"> -->
-                        <select name="levelID" id="label" class="form-select">
+                        <select name="data[]" class="form-select" aria-label="Default select example" required>
                             <option selected>Pilih Level</option>
-                            <option value="Level">Admin</option>
-                            <option value="Level">Dosen</option>
-                            <option value="Level">Mahasiswa</option>
+                            <?php
+                            $query2 = readData($koneksi, "Level");
+                            foreach ($query2 as $row2) {
+                                ?>
+                                <option value="<?= $row2['LevelID']; ?>">
+                                    <?= $row2['Keterangan']; ?>
+                                </option>
+                                <?php
+                            }
+                            ?>
                         </select>
+                    </div>
+                    <div class=" mb-3">
+                        <label for="id" class="col-form-label">Kelas :</label>
+                        <select name="data[]" class="form-select" aria-label="Default select example" required>
+                            <option selected>Pilih Kelas</option>
+                            <?php
+                            $query3 = readData($koneksi, "Kelas");
+                            foreach ($query3 as $row3) {
+                                ?>
+                                <option value="<?= $row3['KelasID']; ?>">
+                                    <?= $row3['NamaKelas']; ?>
+                                </option>
+                                <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class=" mb-3">
+                        <label for="id" class="col-form-label">Username :</label>
+                        <input type="text" name="data[]" class="form-control" id="id">
+                    </div>
+                    <div class=" mb-3">
+                        <label for="id" class="col-form-label">Password :</label>
+                        <input type="password" name="password" class="form-control" id="id">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -153,4 +203,4 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-</script>
+    </script>
